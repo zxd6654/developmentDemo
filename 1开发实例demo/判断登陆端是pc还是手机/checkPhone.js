a01c235151fbbf438662e3c81a805c2defe3960e
zxd6654 <739971915@qq.com>
2020-05-11 09:32:18 +0800

    伪元素和伪类

--- 1开发实例demo/判断登陆端是pc还是手机/checkPhone.js
+++ 1开发实例demo/判断登陆端是pc还是手机/checkPhone.js
@@ -1,19 +1,31 @@
-function checkPhone() {
-    var mobileAgent = new Array("iphone", "ipod", "ipad", "android"/*, "mobile"*/, "blackberry", "webos", "incognito", "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
-    var browser = navigator.userAgent.toLowerCase();
-    var isMobile = false;
-    for (var i = 0; i < mobileAgent.length; i++) { //如果是浏览器是手机版
+/**
+ * 手机端检测跳转到手机端 检查是否是手机端，如果是手机端而且点击过切换到电脑端，这时不会切换到手机
+ */
+ function checkPhone() {
+    let mobileAgent = new Array("iphone", "ipod", "ipad", "android"/*, "mobile"*/, "blackberry", "webos", "incognito", "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
+    let browser = navigator.userAgent.toLowerCase();
+    for (let i = 0; i < mobileAgent.length; i++) { //如果是浏览器是手机版
         if (browser.indexOf(mobileAgent[i]) != -1) {
-            isMobile = true;
-            // 重定向手机端网址
-            if (window.location.href.indexOf("localhost") !== -1) {
-                location.href = "http://localhost:8088/mobile/";
-            }
-            if (window.location.href.indexOf("homing.sg") != -1) {
-                location.href = "http://homing.sg/mobile/";//正式服务器手机端网址
+            let origin = location.origin;
+            let str = location.href;
+            let index = str.indexOf('?');
+
+            if(str.indexOf('/mycenter/orders')!=-1){
+                if (index === -1) {
+                    location.href = origin + "/index.php?route=mobile/index&#/orders";
+                } else {
+                    location.href = origin + "/index.php?route=mobile/index&#/orders" + str.substr(index + 1);
+                }
+            }else{
+                if (index === -1) {
+                    location.href = origin + "/index.php?route=mobile/index&";
+                } else {
+                    location.href = origin + "/index.php?route=mobile/index&" + str.substr(index + 1);
+                }
             }
             break;
         }
     }
 }
+
 checkPhone();
